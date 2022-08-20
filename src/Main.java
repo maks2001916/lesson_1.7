f4a147475c3111133b88f09d7e29e96889702222
maks <mamonov.maksim.20003@gmail.com>
2022-08-20 10:33:58 +0500

    Первое задание выполнено

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        //первое задание
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String lfm = lastName + ' ' + firstName + ' ' + middleName;
+        System.out.println("ФИО сотрудника - " + lfm);
     }
 }
